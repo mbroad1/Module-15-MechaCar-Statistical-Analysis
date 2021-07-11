344978c9d0f867975ffa61e9d8091b0395e0a788
mbroad1 <77813137+mbroad1@users.noreply.github.com>
2021-07-11 14:24:37 -0700

    Deliverable 3: images of results and t-test script added

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,5 +9,12 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspensioncoil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 #Create total_summary by summarizing suspensioncoil_table
 total_summary <- suspensioncoil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
-#Groupby Manufacturing_Lot and then summarize
+#Group by Manufacturing_Lot and then summarize
 lot_summary <- suspensioncoil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
+
+#T-test for comparing all the lots to the population mean
+t.test(x=suspensioncoil_table$PSI,mu=1500) #compare sample versus population means
+#T-tests comparing each lot to the population mean
+t.test(subset(suspensioncoil_table,Manufacturing_Lot == "Lot1")$PSI,mu=1500)
+t.test(subset(suspensioncoil_table,Manufacturing_Lot == "Lot2")$PSI,mu=1500)
+t.test(subset(suspensioncoil_table,Manufacturing_Lot == "Lot3")$PSI,mu=1500)
